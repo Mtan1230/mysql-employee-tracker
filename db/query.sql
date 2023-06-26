4657ead74c92630cc4d53cced48a680f96ebe292
Mtan1230 <m.tan1230@gmail.com>
2023-06-26 15:40:38 -0400

    add query.sql

---
+++ db/query.sql
@@ -0,0 +1,7 @@
+SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, 
+    CONCAT(m.first_name, ' ', m.last_name) AS manager
+FROM employee e
+LEFT JOIN role r ON e.role_id = r.id
+LEFT JOIN department d ON r.department_id = d.id
+LEFT JOIN employee m ON e.manager_id = m.id
+ORDER BY e.id;
