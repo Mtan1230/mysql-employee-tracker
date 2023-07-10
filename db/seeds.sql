4d2475d4fcb194f5ff2b99d50cf5323cdcb76c39
Mtan1230 <m.tan1230@gmail.com>
2023-07-10 01:10:28 -0400

    updated seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,14 +5,14 @@ INSERT INTO department (name) VALUES
   ('Legal');
 
 INSERT INTO role (title, salary, department_id) VALUES
-  ('Sales Manager', 90000.00, 1),
-  ('Sales Representative', 65000.00, 1),
-  ('Lead Engineer', 120000.00, 2),
-  ('Software Engineer', 90000.00, 2),
-  ('Account Manager', 95000.00, 3),
-  ('Accountant', 60000.00, 3),
-  ('Legal Team Lead', 15000.00, 4),
-  ('Legal Assistant', 10000.00, 4),
+  ('Sales Lead', 100000.00, 1),
+  ('Salesperson', 80000.00, 1),
+  ('Lead Engineer', 150000.00, 2),
+  ('Software Engineer', 120000.00, 2),
+  ('Account Manager', 160000.00, 3),
+  ('Accountant', 125000.00, 3),
+  ('Legal Team Lead', 250000.00, 4),
+  ('Lawyer', 190000.00, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
   ('John', 'Doe', 1, null),
@@ -22,4 +22,4 @@ INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
   ('David', 'Brown', 5, null),
   ('Emily', 'Taylor', 6, 5),
   ('Michael', 'Davis', 7, null),
-  ('Jennifer', 'Miller', 8, 7),
+  ('Jennifer', 'Miller', 8, 7);
