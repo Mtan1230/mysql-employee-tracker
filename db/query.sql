3a801b7cae4f44a058686e02ad094a8a86f8438f
Mtan1230 <m.tan1230@gmail.com>
2023-07-10 04:16:42 -0400

    improve and add comment to codebase

--- db/query.sql
+++ db/query.sql
@@ -1,3 +1,4 @@
+-- View all Employees
 SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, 
     CONCAT(m.first_name, ' ', m.last_name) AS manager
 FROM employee e
@@ -6,20 +7,7 @@ LEFT JOIN department d ON r.department_id = d.id
 LEFT JOIN employee m ON e.manager_id = m.id
 ORDER BY e.id;
 
-
-SELECT CONCAT(first_name, ' ', last_name) AS manager
-FROM employee
-WHERE manager_id IS NULL;
-
-
-SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, 
-    CONCAT(m.first_name, ' ', m.last_name) AS manager
-FROM employee e
-LEFT JOIN role r ON e.role_id = r.id
-LEFT JOIN department d ON r.department_id = d.id
-LEFT JOIN employee m ON e.manager_id = m.id
-ORDER BY m.id;
-
+-- View employees by manager
 SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary 
 FROM employee e
 LEFT JOIN role r ON e.role_id = r.id
@@ -27,6 +15,7 @@ LEFT JOIN department d ON r.department_id = d.id
 LEFT JOIN employee m ON e.manager_id = m.id
 WHERE CONCAT(m.first_name, ' ', m.last_name) = 'John Doe';
 
+-- View employees by department
 SELECT e.id, e.first_name, e.last_name, r.title, r.salary,
     CONCAT(m.first_name, ' ', m.last_name) AS manager
 FROM employee e
@@ -35,6 +24,7 @@ LEFT JOIN department d ON r.department_id = d.id
 LEFT JOIN employee m ON e.manager_id = m.id
 WHERE d.name = 'Sales';
 
+-- View budge group by department
 SELECT d.name AS department, SUM(r.salary) AS total_salary
 FROM employee e
 LEFT JOIN role r ON e.role_id = r.id
