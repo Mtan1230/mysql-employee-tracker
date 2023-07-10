b0856933dfdec50c4766f8e8970af55373c6315d
Mtan1230 <m.tan1230@gmail.com>
2023-07-10 03:31:31 -0400

    add code to VIEW

--- db/query.sql
+++ db/query.sql
@@ -19,3 +19,24 @@ LEFT JOIN role r ON e.role_id = r.id
 LEFT JOIN department d ON r.department_id = d.id
 LEFT JOIN employee m ON e.manager_id = m.id
 ORDER BY m.id;
+
+SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary 
+FROM employee e
+LEFT JOIN role r ON e.role_id = r.id
+LEFT JOIN department d ON r.department_id = d.id
+LEFT JOIN employee m ON e.manager_id = m.id
+WHERE CONCAT(m.first_name, ' ', m.last_name) = 'John Doe';
+
+SELECT e.id, e.first_name, e.last_name, r.title, r.salary,
+    CONCAT(m.first_name, ' ', m.last_name) AS manager
+FROM employee e
+LEFT JOIN role r ON e.role_id = r.id
+LEFT JOIN department d ON r.department_id = d.id
+LEFT JOIN employee m ON e.manager_id = m.id
+WHERE d.name = 'Sales';
+
+SELECT d.name AS department, SUM(r.salary) AS total_salary
+FROM employee e
+LEFT JOIN role r ON e.role_id = r.id
+LEFT JOIN department d ON r.department_id = d.id
+GROUP BY d.name;
